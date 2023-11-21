0180cd36edec9c85ffd2a7d2cea21983968a0832
iloElias <murilo7456@outlook.com>
2023-11-20 21:40:05 -0300

    chore: minor chnges at user login and signin

--- src/App.js
+++ src/App.js
@@ -6,11 +6,22 @@ import PopupTemplate from "./Components/PopupTemplate";
 import userImg from "./images/iconUser.png"
 
 function App() {
-  // const [user, setUser] = useState()
+  const [user, setUser] = useState();
+  const [userID, setUserID] = useState();
+  const [userLastName, setUserLastName] = useState();
+  const [userEmail, setUserEmail] = useState();
+  const [userPassword, setUserPassword] = useState();
+  const [userCPF, setUserCPF] = useState();
+  const [userTel, setUserTel] = useState();
+
+  let userInfo = { user, userID, userEmail, userPassword, userLastName, userCPF, userTel };
+  let userSetInfo = { setUser, setUserID, setUserEmail, setUserPassword, setUserLastName, setUserCPF, setUserTel };
+
   const [isModalOpen, setModalOpen] = useState(false);
   const [modalType, setModalType] = useState(null);
 
   const openModal = (type) => {
+    closeModal();
     setModalType(type);
     setModalOpen(true);
   };
@@ -26,7 +37,7 @@ function App() {
       <div id="modal-root" className={`modal ${isModalOpen ? "modal-open" : ""}`}>
         {isModalOpen ? (
           <PopupTemplate onClose={closeModal} title={modalType === "signin" ? "Faça seu cadastro" : "Entrar na sua conta"} dialogImg={userImg}
-            component={modalType === "signin" ? <SingInInputs /> : <LoginInputs />} />
+            component={modalType === "signin" ? <SingInInputs openModal={openModal} userInfo={userInfo} userSetInfo={userSetInfo} /> : <LoginInputs openModal={openModal} userInfo={userInfo} userSetInfo={userSetInfo} />} />
         ) : (
           <></>
         )}
@@ -39,7 +50,6 @@ function App() {
         <p>
           Esta é a minha primeira aplicação React
         </p>
-
       </div>
     </>
   );
--- src/Components/AcountInputs.js
+++ src/Components/AcountInputs.js
@@ -1,20 +1,21 @@
-import React, { useRef } from "react";
 
 function SingInInputs(params) {
-    const ref = useRef();
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    };
+
+    const openModal = () => {
+        params.openModal("login");
+    }
 
     return (
-        <form className="sing-in-form">
+        <form onSubmit={handleSubmit} className="sing-in-form">
             <div className="input-flex">
                 <input type="text" placeholder="Nome" />
                 <input type="text" placeholder="Sobrenome" />
             </div>
             <div className="input-flex">
-                <input type="text" placeholder="Data de nascimento"
-                    ref={ref}
-                    onChange={(e) => console.log(e.target.value)}
-                    onFocus={() => (ref.current.type = "date")}
-                    onBlur={() => (ref.current.type = "text")} />
+                <input type="text" placeholder="CPF" />
                 <input type="tel" placeholder="Telefone" />
             </div>
             <input type="email" placeholder="E-mail" />
@@ -25,21 +26,29 @@ function SingInInputs(params) {
 
             <div className="input-space-between">
                 <input type="submit" value="Realizar cadastro" />
-                <a href="/">Já tenho conta...</a>
+                <button onClick={openModal}>Já tenho conta...</button>
             </div>
         </form>
     )
 }
 
 function LoginInputs(params) {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    };
+
+    const openModal = () => {
+        params.openModal("signin");
+    }
+
     return (
-        <form className="sing-in-form">
+        <form onSubmit={handleSubmit} className="sing-in-form">
             <input className="min-width-input" type="email" placeholder="E-mail" />
             <input className="min-width-input" type="password" placeholder="Senha" />
 
             <div className="input-space-between">
                 <input type="submit" value="Fazer Login" />
-                <a href="/">Sou novo por aqui...</a>
+                <button onClick={openModal}>Sou novo por aqui...</button>
             </div>
         </form>
     )
