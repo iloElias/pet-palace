0d58f3c0d581916aeee8456443c22f6409fa52a7
muriloElias <murilo7456@outlook.com>
2023-12-05 14:51:11 -0300

    chore: render tests complete

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,30 @@
-import { render, screen } from '@testing-library/react';
-import { App, NotAllowed } from './App';
+import { render, screen } from "@testing-library/react";
+import { App, NotAllowed } from "./App";
+import Header from "./Components/Header";
+import PopupTemplate from "./Components/PopupTemplate";
 
-test('renders seja bem vindo link', () => {
+test("renders PopupTemplate component", () => {
+  render(
+    <PopupTemplate onClose={() => {}} title="Test Popup">
+      <p>Content goes here...</p>
+    </PopupTemplate>
+  );
+
+  const popupTitle = screen.getByText(/Test Popup/i);
+  const contentText = screen.getByText(/Content goes here/i);
+
+  expect(popupTitle).toBeInTheDocument();
+  expect(contentText).toBeInTheDocument();
+});
+
+test("renders seja bem vindo link", () => {
   render(<App />);
   const linkElement = screen.getByText(/Seja bem-vindo ao Pet Palace/i);
   expect(linkElement).toBeInTheDocument();
 });
 
-test('renders nao permitido component', () => {
+test("renders nao permitido component", () => {
   render(<NotAllowed />);
-  const headerElement = screen.getByText(/Página não encontrada/i);
+  const headerElement = screen.getByText(/403 - Página não pode ser acessada/i);
   expect(headerElement).toBeInTheDocument();
 });
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,18 +1,23 @@
-import Logo from "./Logo"
-import Menu from "./Menu"
-import { Link } from "react-router-dom"
+import Logo from "./Logo";
+import Menu from "./Menu";
+import { Link } from "react-router-dom";
 
-function Header({ openSignInModal, openLoginModal, user, handleLogout}) {
-    return (
-        <header>
-            <Link to="/" className="company-logo" >
-                <Logo />
-            </Link>
-            <div className="menu">
-                <Menu openSignInModal={openSignInModal} openLoginModal={openLoginModal} user={user} handleLogout={handleLogout} />
-            </div>
-        </header>
-    );
+function Header({ openSignInModal, openLoginModal, user, handleLogout }) {
+  return (
+    <header>
+      <Link to="/" className="company-logo">
+        <Logo />
+      </Link>
+      <div className="menu">
+        <Menu
+          openSignInModal={openSignInModal}
+          openLoginModal={openLoginModal}
+          user={user}
+          handleLogout={handleLogout}
+        />
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
